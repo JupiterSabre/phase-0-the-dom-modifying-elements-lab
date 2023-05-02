8b6e36a4de1d147bd17553b9f55642ce46008657
Zulai Romero <zulairomero@gmail.com>
2023-05-02 12:01:59 -0400

    script for basic DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+
+const sumElem = document.getElementById('main');
+sumElem.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = 'Zulai is the champion';
+document.body.append(newHeader);
+
+
+
+
+// document.body.append(newHeader);
+
+
+
+
+
+
